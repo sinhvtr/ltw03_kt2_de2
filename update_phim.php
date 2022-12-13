de661fc40010e9af7f3dd6393cd6874c07c98ed0
sinhvtr <sinhvtr@gmail.com>
2022-12-13 21:48:19 +0700

    add update_phim

---
+++ update_phim.php
@@ -0,0 +1,12 @@
+<?php
+require("header.php");
+?>
+<div class="col-sm-9">
+    <h1>Update thông tin phim</h1>
+    <form class="form form-horizontal" method="post" action="#">
+        
+    </form>
+</div>
+<?php
+require("footer.php");
+?>
